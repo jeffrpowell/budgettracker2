f4155e24e8ca710645aaea624051bd4b30a54484
jeffrpowell <jeff.powell@byu.edu>
2015-05-25 18:21:17 -0600

    Running total shown on the paycheck page.

--- app/components/paycheck/paycheck.js
+++ app/components/paycheck/paycheck.js
@@ -6,14 +6,33 @@ angular.module('budgetTracker.paycheck', ['budgetTracker.services'])
 		$scope.categories = Categories.get();
 		$scope.changeDate = function(newDate){
 			$scope.date = newDate;
+			$scope.runningTotal = {};
 			if ($scope.unbind){
 				$scope.unbind();
 			}
 			Budget.getBudgetForMonth(newDate).$bindTo($scope, "budget").then(function(unbind){
 				$scope.unbind = unbind;
+				$scope.refreshTotals();
+			});
+		};
+		$scope.refreshTotals = function(){
+			var total = $scope.budget.income;
+			angular.forEach($scope.budget.categories, function(category, categoryKey){
+				angular.forEach(category.envelopes, function(envelope, envelopeKey){
+					var envelopeAllocation = 0;
+					if (envelope.allocation){
+						envelopeAllocation = envelope.allocation;
+					}
+					else if (envelope.suggestion){
+						envelopeAllocation = envelope.suggestion;
+					}
+					total -= envelopeAllocation;
+					$scope.runningTotal[envelopeKey] = total;
+				});
 			});
 		};
 		$scope.updateNextMonthsSuggestion = function(category, envelope){
 			Budget.updateNextMonthsSuggestion($scope.date, category, envelope, $scope.budget.categories[category].envelopes[envelope].allocation);
+			$scope.refreshTotals();
 		};
 	}]);
